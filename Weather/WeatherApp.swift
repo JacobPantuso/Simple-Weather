e4bb54b7a6715420e7bf05b9910abcd0b68f2172
Jacob Pantuso <jacobpantuso@outlook.com>
2022-03-11 07:44:15 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jacob Pantuso on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
